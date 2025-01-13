e5a4e979e84873f6cb0dd0a549642d724d6b6fdd
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-01-13 14:32:01 -0800

    default to data.frame class

--- R/LinkedDataTable.R
+++ R/LinkedDataTable.R
@@ -1,23 +1,35 @@
 #' @export
 #' @exportClass LinkedDataTable
-LinkedDataTable <- function(x = list(), idcol = NULL, primary_table = 1, ...) {
+LinkedDataTable <- function(x = list(),
+														idcol = NULL,
+														primary_table = 1,
+														dfclass = getOption("LinkedDataTable_data.frame_class", default = "data.frame"),
+														...) {
+
 	if (is.data.frame(x) || !is.list(x)) {
 		x <- list(x = x)
 	}
+
 	if (length(x) >= 1 && (missing(idcol) || is.null(idcol))) {
 		idcol <- colnames(x[[1]])[1]
 	} else if (is.null(idcol)) {
 		idcol <- NA_character_
 	}
-	structure(lapply(x, .to.data.table),
+
+	structure(lapply(x, .to.data.table, dfclass = dfclass),
 						idcol = idcol,
 						primary_table = primary_table,
+						dfclass = dfclass,
 						class = "LinkedDataTable")
 }
 
-.to.data.table <- function(x) {
-	if (!inherits(x, "data.table")) {
-		x <- data.table::data.table(x)
+.to.data.table <- function(x, dfclass = "data.frame") {
+	if (!inherits(x, dfclass)) {
+		if (dfclass == "data.frame") {
+			return(data.frame(res))
+		} else {
+			return(data.table::data.table(x))
+		}
 	}
 	x
 }
@@ -26,12 +38,14 @@ LinkedDataTable <- function(x = list(), idcol = NULL, primary_table = 1, ...) {
 print.LinkedDataTable <- function(x, ...) {
 	cn <- names(x)
 	idc <- attr(x, "idcol")
+
 	if (is.null(cn)) {
 		cn <- "<null>"
 	} else {
 	  nr <- sapply(x, nrow)
 		cn <- paste0(cn, "<", nr, ">")
 	}
+
 	cat("LinkedDataTable", paste0("(ID: ", idc, ")"), "with", length(x), "tables:\n  ")
 	cat(cn, sep = ", ")
 }
@@ -57,10 +71,14 @@ print.LinkedDataTable <- function(x, ...) {
 #' @export
 `+.LinkedDataTable` <- function(x, y) {
 	stopifnot(attr(x, "idcol") == attr(y, "idcol"))
+
 	yn <- names(y)
 	xn <- names(x)
+
 	x[yn] <- lapply(yn, function(n) {
-		data.table::rbindlist(list(x[[n]], y[[n]]), fill = TRUE)
+		res <- data.table::rbindlist(list(x[[n]], y[[n]]), fill = TRUE)
+		.to.data.table(res, dfclass = attr(x, 'dfclass'))
 	})
+
 	LinkedDataTable(x, attr(x, "idcol"))
 }
